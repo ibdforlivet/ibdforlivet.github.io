a62f6a696cf5bb12ee0cbeb35321217ba4d6aa39
Morten Hoffmann <moho@semler.dk>
2025-09-25 11:54:23 +0200

    minify images

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      mozjpeg: {
+        quality: 80
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      webp: {
+        quality: 80
+      }
+    })
+  ],
   base: '/',
   publicDir: 'public',
   build: {
